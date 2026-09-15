9210cec5370319b69dd0487fb30ca176558a207a
Zaima Melati <zaimamelati@gmail.com>
2026-09-15 20:46:09 +0700

    Fix project search

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,15 @@
 import ProjectSearch from "../../components/ProjectSearch";
 
 export default function ProjectsPage() {
+  const handleSearch = (value: string) => {
+    console.log("Search:", value);
+  };
+  
   return (
     <main>
       <h1>Katalog Proyek</h1>
 
-      <ProjectSearch />
+      <ProjectSearch onSearch={handleSearch} />
 
       {/* kartu-kartu proyek */}
     </main>
